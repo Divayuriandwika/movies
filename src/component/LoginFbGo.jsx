8f9da0c962b8f58e815ee233b8585f53a7cc9bc1
Diva Yurian DWika <divayuriandwika@gmail.com>
2020-08-02 14:40:26 +0700

    trying to input login fb and google

--- src/component/LoginFbGo.jsx
+++ src/component/LoginFbGo.jsx
@@ -1,37 +1,37 @@
-// import React from 'react';
-// import FacebookLogin from 'react-facebook-login';
-// import GoogleLogin from 'react-google-login';
+import React from 'react';
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
 
 
-// export default function App () {
+export default function App () {
 
-//     const responseFacebook = (response) => {
-//       console.log(response);
-//     }
+    const responseFacebook = (response) => {
+      console.log(response);
+    }
 
-//     const responseGoogle = (response) => {
-//       console.log(response);
-//     }
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
 
-//     return (
-//       <div className="App">
+    return (
+      <div className="App">
 
-//       <FacebookLogin
-//         appId="670629370188855"
-//         fields="name,email,password"
-//         callback={responseFacebook}
-//       />
-//       <br />
-//       <br />
+      <FacebookLogin
+        appId="670629370188855"
+        fields="name,email,password"
+        callback={responseFacebook}
+      />
+      <br />
+      <br />
 
 
-//       <GoogleLogin
-//         clientId="" //CLIENTID NOT CREATED YET
-//         buttonText="LOGIN WITH GOOGLE"
-//         onSuccess={responseGoogle}
-//         onFailure={responseGoogle}
-//       />
+      <GoogleLogin
+        clientId="" //CLIENTID NOT CREATED YET
+        buttonText="LOGIN WITH GOOGLE"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
 
-//       </div>
-//     );
-//   }
+      </div>
+    );
+  }
